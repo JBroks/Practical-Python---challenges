572cba93faca211940fd4f684381c3218d82792d
JBroks <asia.ostr@gmail.com>
2019-08-06 09:19:16 +0000

    test_between added

--- byotest.py
+++ byotest.py
@@ -18,4 +18,7 @@ def test_is_in(collection, item):
 def test_not_in(collection, item):
     assert item not in collection, "{0} does contain {1}".format(collection, item)
 
-test_not_in([0,1,2,3,4], number_of_evens([1,2,3]))
+def test_between(value,lower_limit,upper_limit):
+    assert value <= upper_limit and value >= lower_limit, "{0} is not between {1} and {2}".format(value, lower_limit, upper_limit)
+
+test_between(1,2,4)
