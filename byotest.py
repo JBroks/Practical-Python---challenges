7530e27225397f7045b0d1659f0ddb9fd7c42db8
JBroks <asia.ostr@gmail.com>
2019-08-06 09:30:09 +0000

    Comments added

--- byotest.py
+++ byotest.py
@@ -3,7 +3,7 @@ def number_of_evens(numbers):
 
 def count_numbers(numbers):
     return False
-
+    
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
@@ -21,4 +21,16 @@ def test_not_in(collection, item):
 def test_between(value,lower_limit,upper_limit):
     assert value <= upper_limit and value >= lower_limit, "{0} is not between {1} and {2}".format(value, lower_limit, upper_limit)
 
+
+# Test to fail the `test_not_equal` function
+#test_not_equal(count_numbers([1,2,3]), False)
+
+# Test to fail the `test_are_equal` function
+#test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+
+# Test to fail the `test_not_in` function
+#test_not_in([0,1,2,3,4], number_of_evens([1,2,3]))
+
+# Test to fail the `test_between` function
 test_between(1,2,4)
+
