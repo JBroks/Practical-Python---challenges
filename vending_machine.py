139adfc3e934dbd8825fa51c89777862b2160455
JBroks <asia.ostr@gmail.com>
2019-08-06 11:18:05 +0000

    Function changed to replace list with dictionary

--- vending_machine.py
+++ vending_machine.py
@@ -1,16 +1,18 @@
 from byotest import *
 
-euro_coins = [100, 50, 20, 10, 5, 2, 1]
-usd_coins = [100, 50, 25, 10, 5, 1]
+euro_coins = {1: 20, 2: 20, 5: 20, 10: 20, 20: 20, 50: 20, 100: 20}
+usd_coins = {1: 20, 5: 20, 10: 20, 25: 20, 50: 20, 100: 20}
 
 def get_change(amount, coins = euro_coins):
     change = []
-    for coin in coins:
-        while coin <= amount:
-            amount -= coin
-            change.append(coin)
+    for denomination in sorted(coins.keys(), reverse=True):
+        while denomination <= amount and coins[denomination] > 0:
+            amount -= denomination
+            change.append(denomination)
+    if amount != 0:
+        raise Exception("Insufficient coins to give change.")
     return change
-    
+ 
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1), [1])
 test_are_equal(get_change(2), [2])
@@ -23,4 +25,6 @@ test_are_equal(get_change(3), [2,1])
 test_are_equal(get_change(7), [5,2])
 test_are_equal(get_change(9), [5,2,2])
 test_are_equal(get_change(35, usd_coins), [25,10])
+
+
 print("All test pass!")
